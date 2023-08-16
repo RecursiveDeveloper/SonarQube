513d507fc5f4a6f84a6debf991bb98863b2edad1
JHJORTIZ <JHJORTIZ@bancolombia.com.co>
2023-08-16 09:52:52 -0500

    azure-pipeline secondary and main templates added

--- Iac/Terraform/provider.tf
+++ Iac/Terraform/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "5.3.0"
     }
   }
+
+  backend "s3" {
+    bucket = "terraform-state-sonarq"
+    key    = "path/state/key"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
